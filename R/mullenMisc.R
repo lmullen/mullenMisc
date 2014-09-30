72e13c10b89e767166024146835c2e90804fd43e
Lincoln Mullen <lincoln@lincolnmullen.com>
2014-09-30 14:12:12 -0400

    Fix problems with importing namespaces

--- R/mullenMisc.R
+++ R/mullenMisc.R
@@ -1,5 +1,10 @@
 #' A package of functions that I use repeatedly
-#' 
+#'
 #' @docType package
 #' @name mullenMisc
+#' @import gridExtra
+#' @import ggplot2
+#' @import knitr
+#' @import dplyr
+#' @import grid
 NULL
